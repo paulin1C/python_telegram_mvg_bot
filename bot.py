d0ede863dac6abe0947971d9a6f828a6b2ce786a
Paul <paul@mos.fritz.box>
2016-08-25 18:53:23 +0200

    basicly added complete route funtionality

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-import logging, os, time, subprocess, re, sys
+import logging, os, time, subprocess, re, sys, time
 sys.path.append('python_mvg_departures')
 from datetime import *
 from telegram import *
@@ -18,12 +18,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Hi, send me the name of a public transport station or just share your location.')
+    bot.sendMessage(update.message.chat_id, text='Hi, send me the name of a public transport station or just share your location.\nUse /help to get more information (for example about how to get journeys details)')
     logger.info('start used by %s', update.message.from_user)
 
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='This bot will send you the departure times of public transport stations in Munich (Germany).')
-    bot.sendMessage(update.message.chat_id, text='Just share your location and select a nearby station or send the name of the station.')
+    #bot.sendMessage(update.message.chat_id, text='This bot will send you the departure times of public transport stations in Munich (Germany).')
+    bot.sendMessage(update.message.chat_id, text='Just share your location and select a nearby station or send the name of the station to see its departures.\n\nYou can get journeys by sending messages like in these examples\nHauptbahnhof nach Ostbahnhof\noder\nMarienpaltz nach Obersendling um 20:00\noder\nOdensplatz nach Siemenswerke bis 21:00\nin the end it is:\nfromStation [nach|to] toStation ([ab|um|bis|at|until] time)')
     logger.info('help used by %s', update.message.from_user)
 
 def gps(bot, update):
@@ -63,8 +63,22 @@ def gps_answer(bot, update):
     sendDepsforStation(bot, update, station_id_str, update.message.message_id)
 
 def msg(bot, update):
-    station = update.message.text
-    sendDepsforStation(bot, update, station)
+    #thanks to @uberardy for these regulare expressions
+    pattern1 = "(?:von )?(.+) (nach|to) (.*)"
+    pattern2 = "(?:von )?(.+) (nach|to) (.*)(?: (um|ab|bis|at|until) ([0-9]{1,2}:?[0-9]{2}))"
+    result1 = re.match(pattern1, update.message.text)
+    if result1 == None:
+        station = update.message.text
+        sendDepsforStation(bot, update, station)
+    else:
+        result2 = re.match(pattern2, update.message.text)
+        if result2 == None:
+            result = result1
+            b_time = False
+        else:
+            result = result2
+            b_time = True
+        sendJourneys(bot, update, result, b_time)
 
 def sendDepsforStation(bot, update, station_raw, message_id = -1):
     if message_id > -1:
@@ -92,7 +106,6 @@ def sendDepsforStation(bot, update, station_raw, message_id = -1):
             now = datetime.datetime.now()
             header="minutes, service, destination"
             body = ""
-            service = {'t': "", 'u': "U", 'b': "", 's': "S"}
 
             times=[]
             products=[]
@@ -102,11 +115,8 @@ def sendDepsforStation(bot, update, station_raw, message_id = -1):
                 len_dTM = len(str(departure['departureTimeMinutes']))
                 if not len_dTM > 3:
                     times.append(str(departure['departureTimeMinutes']))
-                    try:
-                        product = service[departure['product']]
-                    except:
-                        product = departure['product']
-                    products.append(product+str(departure['label']))
+                    product = build_label(departure['product'], departure['label'])
+                    products.append(product)
                     destinations.append(departure['destination'])
                     i=i+1
 
@@ -163,6 +173,63 @@ def sendDepsforStation(bot, update, station_raw, message_id = -1):
             else:
                 bot.sendMessage(update.message.chat_id, text=msg, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
 
+def sendJourneys(bot, update, result, b_time):
+    try:
+        from_station_id = get_id_for_station(result.group(1))
+        to_station_id = get_id_for_station(result.group(3))
+    except:
+        bot.sendMessage(update.message.chat_id, text="Station(s) not found :(")
+        logger.warn('Not matching station name in journeys used by %s', update.message.from_user)
+    else:
+        arrival_time = False
+        i_time = datetime_to_mvgtime(datetime.datetime.now())
+        if b_time:
+            if result.group(4) in ["bis","until"]:
+                arrival_time = True
+            try:
+                i_time = datetime_to_mvgtime(datetime.datetime.combine(datetime.datetime.now(), datetime.datetime.strptime(result.group(5), "%H:%M").time()))
+            except:
+                bot.sendMessage(update.message.chat_id, text="invalid time :(\nplease use hh:mm format\nwill use current time now")
+                logger.warn('invalid time used by %s', update.message.from_user)
+        route = get_route(from_station_id, to_station_id, i_time, arrival_time)
+        msg = buildRouteMsg(route)
+        bot.sendMessage(update.message.chat_id, text=msg, parse_mode=ParseMode.HTML)
+        logger.info('journey from %s to %s sent to %s, b_time=%s', str(from_station_id), str(to_station_id), update.message.from_user, str(b_time))
+
+def buildRouteMsg(route):
+    body=""
+    counter=0
+    for option in route['connectionList']:
+        counter +=1
+        body += "\n"
+        body += "Option " + str(counter) + ":\n"
+        # body += mvgtime_to_hrs(option['departure']) + " - " + option['from']['name'] + "\n"
+        for part in option['connectionPartList']:
+            body += mvgtime_to_hrs(part['departure']) + " - " + part['from']['name'] + "\n"
+            if part['connectionPartType'] == "FOOTWAY":
+                # lat = str(part['to']['latitude'])
+                # lon = str(part['to']['longitude'])
+                # station_name = part['to']['name']
+                # link = MessageEntity("url", 20, str("geo:"+lat+","+lon))
+                # bot.sendMessage(159521737, text=link)
+                body += u"      walk to station\n"
+            else:
+                body += "      " + build_label(part['product'], part['label']) + " " + part['destination'] + "\n"
+            body += mvgtime_to_hrs(part['arrival']) + " - " + part['to']['name'] + "\n"
+        # body += mvgtime_to_hrs(option['arrival']) + " - " + option['to']['name'] + "\n"
+    msg=body
+
+    return msg
+
+def build_label(part1,part2):
+    service = {'t': "", 'u': "U", 'b': "", 's': "S"}
+    try:
+        label = service[part1]
+    except:
+        label = part1
+    label += str(part2)
+    return label
+
 def escape_markdown(text):
     """Helper function to escape telegram markup symbols"""
     escape_chars = '\*_`\['
@@ -174,9 +241,17 @@ def addspaces(n, string=""):
         n=n-1
     return string
 
-def wasistdas(bot, update):
+def wasistdas(bdaot, update):
     bot.sendMessage(update.message.chat_id, text='All stations have unique station ids. They can be used instead of a station name. For example, you can write 5 instead of Ostbahnhof.')
     logger.info('wasistdas used by %s', update.message.from_user)
+
+def mvgtime_to_hrs(time):
+    time = datetime.datetime.fromtimestamp(time/1000).strftime("%H:%M")
+    return time
+
+def datetime_to_mvgtime(dtime):
+    time = int(dtime.strftime("%s"))*1000
+    return time
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
